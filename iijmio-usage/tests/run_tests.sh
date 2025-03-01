f811a02cb92efc6ace33be7420f1c6c5fda97224
yananob <yananob@gmail.com>
2025-03-01 15:18:35 +0900

    moved iijmio-usage

--- iijmio-usage/tests/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-echo "Running PHPStan..."
-./vendor/bin/phpstan analyze -c ./phpstan.neon
-
-./vendor/bin/phpunit --colors=auto --display-notices --display-warnings tests/
-# ./vendor/bin/phpunit tests/MyHelloTest.php
